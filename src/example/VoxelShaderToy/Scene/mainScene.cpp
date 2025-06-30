0c69f2c826ae2caf2e9b033697a96f20a84be0c5
Sungun No <nors.nsg@gmail.com>
2025-06-30 20:51:20 +0900

    ex/vst: add main scene

---
+++ src/example/VoxelShaderToy/Scene/mainScene.cpp
@@ -0,0 +1,9 @@
+#include "mainScene.h"
+#include "Entity/user.h"
+#include "Entity/cube.h"
+
+vst::MainScene::MainScene()
+{
+    // user_ = std::make_unique<User>();
+    // entities_.emplace_back(std::make_unique<Cube>());
+}
